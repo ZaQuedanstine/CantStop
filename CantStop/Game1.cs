2f098be734c46b6ad58e0333d823f4059bc28ff6
Abraham Kays <abrahamk@ksu.edu>
2022-03-25 22:33:18 -0500

    got tiled working for the most part

--- CantStop/Game1.cs
+++ CantStop/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.IO;
+using Squared.Tiled;
 
 namespace CantStop
 {
@@ -8,12 +10,16 @@ namespace CantStop
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Map _map;
+        private Vector2 _viewportPosition;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            _graphics.PreferredBackBufferWidth = 1536;
+            _graphics.PreferredBackBufferHeight = 768;
         }
 
         protected override void Initialize()
@@ -28,6 +34,7 @@ namespace CantStop
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            _map = Map.Load(Path.Combine(Content.RootDirectory, "level1.tmx"), Content);
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,7 +52,9 @@ namespace CantStop
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            _map.Draw(_spriteBatch, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), _viewportPosition);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
